cd698b9a9f9091293f60c34d401db3971abb9265
Coin-233 <coin233mail@gmail.com>
2024-09-08 00:46:30 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+GIST_ID = os.getenv('GIST_ID')
+GH_TOKEN = os.getenv('GH_TOKEN')
+BGMTV_USER = os.getenv('BGMTV_USER')
+
+HEADERS = {
+    'User-Agent': 'Coin-233/bgm-box (https://github.com/Coin-233/bgm-box)'
+}
+
+def fetch_bgm_info(username):
+    api_url = f'https://api.bgm.tv/v0/users/{username}'
+    api_response = requests.get(api_url, headers=HEADERS)
+    user_data = api_response.json()
+    nickname = user_data.get('nickname', username)
+
+    user_url = f'https://bgm.tv/user/{username}'
+    response = requests.get(user_url, headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    info = {
+        'fav': soup.find('div', class_='item').find('span', class_='num').text,
+        'pc': soup.find('div', class_='item green').find('span', class_='num').text,
+        'pcr': soup.find('div', class_='item blue').find('span', class_='num').text,
+        'gp': soup.find('div', class_='item orange').find('span', class_='num').text,
+        'sd': soup.find('div', class_='item purple').find('span', class_='num').text,
+        'rat': soup.find('div', class_='item sky').find('span', class_='num').text
+    }
+
+    return info, nickname
+
+def update_gist(info, nickname):
+    gist_url = f'https://api.github.com/gists/{GIST_ID}'
+    headers = {
+        'Authorization': f'token {GH_TOKEN}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    content = f"""
+{info['fav']} 收藏
+{info['pc']} 完成
+{info['pcr']} 完成率
+{info['gp']} 平均分
+{info['sd']} 标准差
+{info['rat']} 评分数
+"""
+    data = {
+        "files": {
+            f"📺{nickname}'s bangumi stats": {
+                "content": content.strip()
+            }
+        }
+    }
+    response = requests.patch(gist_url, json=data, headers=headers)
+
+if __name__ == '__main__':
+    user_info, nickname = fetch_bgm_info(BGMTV_USER)
+    update_gist(user_info, nickname)
